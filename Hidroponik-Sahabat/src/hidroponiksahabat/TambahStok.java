d32f1010bdb51a1b7bdc52840f9e191ec1370dfa
yosuatan11 <yosua.tan1198@gmail.com>
2020-04-08 18:26:56 +0700

    memperbaiki fungsi tombol hapus dan selesai pada tambah stok

--- Hidroponik-Sahabat/src/hidroponiksahabat/TambahStok.java
+++ Hidroponik-Sahabat/src/hidroponiksahabat/TambahStok.java
@@ -49,16 +49,7 @@ public class TambahStok extends javax.swing.JFrame {
         this.setLocationRelativeTo(null);
     }
     
-    public void setStok() {
-        try {
-            Statement stat = (Statement) koneksi.koneksiDB().createStatement();
-        String sql = "Update barang SET jumlah_barang=" + jumlah + " WHERE id_barang=" + id;
-        stat.execute(sql);
-        } catch(SQLException err) {
-            JOptionPane.showMessageDialog(null, err.getMessage());
-        }
-    }
-
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -70,16 +61,13 @@ public class TambahStok extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tableTambah = new javax.swing.JTable();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        jComboBox1 = new javax.swing.JComboBox<String>();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jumlah_stok = new javax.swing.JTextField();
-        jLabel3 = new javax.swing.JLabel();
         harga_satuan = new javax.swing.JTextField();
         selesai = new javax.swing.JButton();
         jLabel5 = new javax.swing.JLabel();
-        no_transaksi = new javax.swing.JTextField();
-        tanggal = new javax.swing.JTextField();
         background1 = new hidroponiksahabat.Background();
         hapus = new javax.swing.JButton();
         tambah = new javax.swing.JButton();
@@ -88,6 +76,8 @@ public class TambahStok extends javax.swing.JFrame {
         jSeparator1 = new javax.swing.JSeparator();
         jButtonMin = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
+        tanggal = new javax.swing.JTextField();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -121,10 +111,6 @@ public class TambahStok extends javax.swing.JFrame {
         jumlah_stok.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         getContentPane().add(jumlah_stok, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 270, 77, -1));
 
-        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel3.setText("@ rupiah");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 270, -1, -1));
-
         harga_satuan.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         harga_satuan.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
         getContentPane().add(harga_satuan, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 270, 82, -1));
@@ -139,15 +125,8 @@ public class TambahStok extends javax.swing.JFrame {
         getContentPane().add(selesai, new org.netbeans.lib.awtextra.AbsoluteConstraints(650, 341, -1, -1));
 
         jLabel5.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        jLabel5.setText("Nomor Transaksi");
+        jLabel5.setText("Tanggal");
         getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 190, -1, -1));
-
-        no_transaksi.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        getContentPane().add(no_transaksi, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 180, 100, -1));
-
-        tanggal.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        tanggal.setText("Tanggal");
-        getContentPane().add(tanggal, new org.netbeans.lib.awtextra.AbsoluteConstraints(246, 180, 140, -1));
 
         background1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
@@ -202,26 +181,37 @@ public class TambahStok extends javax.swing.JFrame {
         jLabel6.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel6.setText("Tambah Stok");
 
+        tanggal.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        tanggal.setText("Tanggal");
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jLabel3.setText("@ rupiah");
+
         javax.swing.GroupLayout background1Layout = new javax.swing.GroupLayout(background1);
         background1.setLayout(background1Layout);
         background1Layout.setHorizontalGroup(
             background1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(background1Layout.createSequentialGroup()
-                .addGap(31, 31, 31)
-                .addComponent(ButtonKembali)
-                .addGap(32, 32, 32)
-                .addComponent(tambah)
-                .addGap(35, 35, 35)
-                .addComponent(hapus)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, background1Layout.createSequentialGroup()
-                .addGap(138, 138, 138)
+                .addGap(245, 245, 245)
                 .addComponent(jLabel6)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 414, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 307, Short.MAX_VALUE)
                 .addComponent(jButtonMin)
                 .addGap(0, 0, 0)
                 .addComponent(jButtonClose, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE))
             .addComponent(jSeparator1, javax.swing.GroupLayout.Alignment.TRAILING)
+            .addGroup(background1Layout.createSequentialGroup()
+                .addGap(31, 31, 31)
+                .addComponent(ButtonKembali)
+                .addGap(32, 32, 32)
+                .addGroup(background1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(background1Layout.createSequentialGroup()
+                        .addComponent(tambah)
+                        .addGap(35, 35, 35)
+                        .addComponent(hapus))
+                    .addGroup(background1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(jLabel3)
+                        .addComponent(tanggal, javax.swing.GroupLayout.PREFERRED_SIZE, 140, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         background1Layout.setVerticalGroup(
             background1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -234,9 +224,13 @@ public class TambahStok extends javax.swing.JFrame {
                         .addGap(8, 8, 8))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, background1Layout.createSequentialGroup()
                         .addComponent(jLabel6)
-                        .addGap(5, 5, 5)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 289, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 136, Short.MAX_VALUE)
+                .addComponent(tanggal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(65, 65, 65)
+                .addComponent(jLabel3)
+                .addGap(48, 48, 48)
                 .addGroup(background1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(background1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(tambah)
@@ -293,34 +287,36 @@ public class TambahStok extends javax.swing.JFrame {
     }//GEN-LAST:event_jButtonMinActionPerformed
 
     private void selesaiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_selesaiActionPerformed
-        if (id != null) {
-            if (JOptionPane.showConfirmDialog(null, "Data Sudah Benar?", "Yakin", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-                try {
-                    Statement stat = (Statement) koneksi.koneksiDB().createStatement();
+        int jumlah = tableTambah.getRowCount();
+               
+        if (jumlah > 0) {
+            try {
+                Statement stat = (Statement) koneksi.koneksiDB().createStatement();
+                for(int i=0; i<jumlah; i++){
+                    id = tableTambah.getValueAt(i, 0).toString();
+                    jumlah = Integer.parseInt(tableTambah.getValueAt(i, 2).toString());
+                    harga = Integer.parseInt(tableTambah.getValueAt(i, 3).toString());
                     String sql = "insert into stock(id_barang, jumlah_stock, harga_stock) values (" + "'" + id + "'," + "'" + jumlah + "'," + harga + ")";
-                    stat.execute(sql);
-
-                    int temp = Integer.parseInt(stok);
-                    if(temp == 0) {
-                        setStok();
-                    } else {
-                        jumlah = jumlah + temp;
-                        setStok();
+                    stat.execute(sql);               
                     }
-                } catch (SQLException err) {
-                    JOptionPane.showMessageDialog(null, err.getMessage());
-                }
-                
-                this.setVisible(false);
-                new TambahStok().setVisible(true);
+                                
+            } catch (SQLException err) {
+                JOptionPane.showMessageDialog(null, err.getMessage());
             }
-        } else {
+            
+          } else {
             JOptionPane.showMessageDialog(null, "Data Masih Kosong!!!", "Peringatan", JOptionPane.WARNING_MESSAGE);
         }
     }//GEN-LAST:event_selesaiActionPerformed
 
     private void hapusActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_hapusActionPerformed
-        model.removeRow(0);
+        DefaultTableModel model = (DefaultTableModel) tableTambah.getModel();
+        int row = tableTambah.getSelectedRow();
+        if(row>=0){
+             if(JOptionPane.showConfirmDialog(null, "Yakin Mau Hapus?","Konfirmasi",JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION){
+                model.removeRow(row);
+             }
+        }
     }//GEN-LAST:event_hapusActionPerformed
 
     /**
@@ -375,7 +371,6 @@ public class TambahStok extends javax.swing.JFrame {
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JTextField jumlah_stok;
-    private javax.swing.JTextField no_transaksi;
     private javax.swing.JButton selesai;
     private javax.swing.JTable tableTambah;
     private javax.swing.JButton tambah;
